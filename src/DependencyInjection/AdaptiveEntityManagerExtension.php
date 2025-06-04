d0a56d4bf50180d7683a6ee5d20f6d46a558ddd6
Ruslan Kabirov <kabirov_rr@rosakhutor.ru>
2025-06-04 19:10:54 +0300

    Fix configuration and initialization of managers

--- src/DependencyInjection/AdaptiveEntityManagerExtension.php
+++ src/DependencyInjection/AdaptiveEntityManagerExtension.php
@@ -24,43 +24,44 @@ class AdaptiveEntityManagerExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
         $config['entities'] = [];
 
-        // Automatic loading of objects from YAML files
-        $entitiesDir = $container->getParameter('kernel.project_dir') . '/config/packages/adaptive_entities/';
-        if (is_dir($entitiesDir)) {
-            $finder = new Finder();
-            $finder->files()->in($entitiesDir)->name('*.yaml');
-            $entities = [];
-            foreach ($finder as $file) {
-                $fileConfig = Yaml::parse($file->getContents());
-                $entities = array_merge($entities, $fileConfig);
-            }
-            $config['entities'] = array_merge($config['entities'], $entities);
+        if (!isset($config['entity_managers'])) {
+            throw new RuntimeException('Parameter "entity_managers" is required for configuration.');
         }
 
-        if (!isset($config['entity_managers'])) {
-            throw new RuntimeException('Parameter "entity_managers" is required for ClassMetadataProvider configuration.');
-        }
-        $container->setParameter('adaptive_entity_manager.entities', $config['entities']);
-        $container->register('adaptive_entity_manager.entity_metadata_provider', EntityClassMetadataProvider::class)
-            ->setArguments([
-                $config['entities'],
-            ]);
-        $container->register('adaptive_entity_manager.adapter_registry', AdapterRegistry::class);
         $container->register('adaptive_entity_manager.manager_registry', ManagerRegistry::class);
+        $container->register('adaptive_entity_manager.adapter_registry', AdapterRegistry::class);
 
         foreach ($config['entity_managers'] as $name => $managerConfig) {
+            $config['entities'][$name] = [];
+            // Automatic loading of objects from YAML files
+            $entitiesDir = $container->getParameter('kernel.project_dir') . '/config/packages/adaptive_entities/';
+            if (is_dir($entitiesDir)) {
+                $finder = new Finder();
+                $finder->files()->in($entitiesDir)->name($name.'.*.yaml');
+                $entities = [];
+                foreach ($finder as $file) {
+                    $fileConfig = Yaml::parse($file->getContents());
+                    $entities = array_merge($entities, $fileConfig);
+                }
+                $config['entities'][$name] = array_merge($config['entities'][$name], $entities);
+            }
+            $container->register('adaptive_entity_manager.'.$name.'_metadata_provider', EntityClassMetadataProvider::class)
+                ->setArguments([
+                    $config['entities'][$name],
+                ]);
+
             $configDefinition = $container->register("adaptive_entity_manager.$name".'_config', Config::class)
                 ->setArguments([
                     $managerConfig['entities_dir'],
                     $managerConfig['entities_namespace'],
-                    $container->getParameter('kernel.cache_dir'),
+                    $container->getParameter('kernel.cache_dir').'/aem/'.$name,
                 ]);
 
             $connectionName = $managerConfig['connection'] ?? null;
             $definition = $container->register("adaptive_entity_manager.$name".'_entity_manager', AdaptiveEntityManager::class)
                 ->setArguments([
                     $configDefinition,
-                    $container->getDefinition('adaptive_entity_manager.entity_metadata_provider'),
+                    $container->getDefinition('adaptive_entity_manager.'.$name.'_metadata_provider'),
                     $container->getDefinition('adaptive_entity_manager.adapter_registry'),
                 ]);
             if ($connectionName !== null) {
